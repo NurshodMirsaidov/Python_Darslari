978019da92ea2e3d4c5b875102b7929d2e20353c
Nurshod <nurshodulugbekovich@gmail.com>
2022-02-28 18:27:55 +0500

    Homeworks has been done

--- function/funksiya_royxat.py
+++ function/funksiya_royxat.py
@@ -14,3 +14,32 @@
 # print(baholar)        
 # print(talabalar)
 
+# Amaliyot
+# 1.Matnlardan iborat ro'yxat qabul qilib,
+# ro'yxatdagi har bir matnning birinchi harfini katta harfga o'zgatiruvchi funksiya yozing.
+
+# def katta_qil(ismlar):
+#     """Funksiyaga ismlar joylangan ro'yxat joylashtirib ko'ring"""
+#     for n in range(len(ismlar)):
+#         ismlar[n] = ismlar[n].title()
+        
+#     return ismlar
+
+# names = ['ali', 'vali', 'hasan', 'husan']
+# yangi_ismlar = katta_qil(names[:])
+# print(yangi_ismlar)
+# print(names)
+
+# Yuqoridagi 1- masalaning ishlanishining ikkinchi yo'li
+
+# def bahola(ismlar):
+#     baholar = {}
+#     for ism in ismlar:
+#         baho = int(input(f"{ism.title()} ning bahosini kiriting: "))
+#         baholar[ism] = baho
+#     return baholar
+
+# talabalar = ['ali', 'vali', 'hasan', 'husan']
+# baholar = bahola(talabalar[:])
+# print(baholar)        
+# print(talabalar)
